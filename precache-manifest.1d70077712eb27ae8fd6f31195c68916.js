2d1e267de5afa99fddeac136821b1da386f6e6c6
Jessicagarg258 <158276794+Jessicagarg258@users.noreply.github.com>
2024-06-26 16:46:48 +0530

    Updates

---
+++ precache-manifest.1d70077712eb27ae8fd6f31195c68916.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baa616c9da3c2237570361c265e995ad",
+    "url": "/textutils/index.html"
+  },
+  {
+    "revision": "a9899cf0d72f0de70dd6",
+    "url": "/textutils/static/css/main.2416104a.chunk.css"
+  },
+  {
+    "revision": "178a5f007b6a2c7c6a20",
+    "url": "/textutils/static/js/2.12d3a3ae.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/textutils/static/js/2.12d3a3ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4c22086003eed385c50",
+    "url": "/textutils/static/js/3.b5bd36bd.chunk.js"
+  },
+  {
+    "revision": "a9899cf0d72f0de70dd6",
+    "url": "/textutils/static/js/main.cb728023.chunk.js"
+  },
+  {
+    "revision": "79ea64f27fef7de3a5b2",
+    "url": "/textutils/static/js/runtime-main.d25e19fa.js"
+  }
+]);
